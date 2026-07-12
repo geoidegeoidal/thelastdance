1ccb8892696fa227fb6cf5d25f52f2f47b0cb875
Jorge UR <jorge.ulloa.roa@gmail.com>
2026-07-11 23:22:35 -0400

    Fix asset loading paths for GitHub Pages

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -7,43 +7,43 @@ export default class BootScene extends Phaser.Scene {
 
   preload() {
     // Load Blue Haired Woman spritesheet
-    this.load.spritesheet('player', '/assets/Blue Haired Woman/blue_haired_woman.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('player', import.meta.env.BASE_URL + 'assets/Blue Haired Woman/blue_haired_woman.png', { frameWidth: 32, frameHeight: 32 });
 
     // Load Tilemap JSON
-    this.load.tilemapTiledJSON('mapa', '/assets/Mapa.json');
+    this.load.tilemapTiledJSON('mapa', import.meta.env.BASE_URL + 'assets/Mapa.json');
     
     // Cargar música de fondo
-    this.load.audio('musica_fondo', '/assets/musica_fondo.mp3');
+    this.load.audio('musica_fondo', import.meta.env.BASE_URL + 'assets/musica_fondo.mp3');
 
       // Load Cat Follower spritesheets
       // Cat 1: Tuxedo (Batman)
-      this.load.spritesheet('cat_tuxedo_idle', '/assets/cats/AllCatsDemo/BatmanCatFree/IdleCatt.png', { frameWidth: 32, frameHeight: 32 });
-      this.load.spritesheet('cat_tuxedo_run', '/assets/cats/AllCatsDemo/BatmanCatFree/JumpCattt.png', { frameWidth: 32, frameHeight: 32 });
+      this.load.spritesheet('cat_tuxedo_idle', import.meta.env.BASE_URL + 'assets/cats/AllCatsDemo/BatmanCatFree/IdleCatt.png', { frameWidth: 32, frameHeight: 32 });
+      this.load.spritesheet('cat_tuxedo_run', import.meta.env.BASE_URL + 'assets/cats/AllCatsDemo/BatmanCatFree/JumpCattt.png', { frameWidth: 32, frameHeight: 32 });
       
       // Cat 2: Carey 1 (Tiger)
-      this.load.spritesheet('cat_carey_idle', '/assets/cats/AllCatsDemo/TigerCatFree/IdleCatt.png', { frameWidth: 32, frameHeight: 32 });
-      this.load.spritesheet('cat_carey_run', '/assets/cats/AllCatsDemo/TigerCatFree/JumpCattt.png', { frameWidth: 32, frameHeight: 32 });
+      this.load.spritesheet('cat_carey_idle', import.meta.env.BASE_URL + 'assets/cats/AllCatsDemo/TigerCatFree/IdleCatt.png', { frameWidth: 32, frameHeight: 32 });
+      this.load.spritesheet('cat_carey_run', import.meta.env.BASE_URL + 'assets/cats/AllCatsDemo/TigerCatFree/JumpCattt.png', { frameWidth: 32, frameHeight: 32 });
       
       // Cat 3: Carey 2 (Brown)
-      this.load.spritesheet('cat_carey2_idle', '/assets/cats/AllCatsDemo/Brown/IdleCattt.png', { frameWidth: 32, frameHeight: 32 });
-      this.load.spritesheet('cat_carey2_run', '/assets/cats/AllCatsDemo/Brown/JumpCatttt.png', { frameWidth: 32, frameHeight: 32 });
+      this.load.spritesheet('cat_carey2_idle', import.meta.env.BASE_URL + 'assets/cats/AllCatsDemo/Brown/IdleCattt.png', { frameWidth: 32, frameHeight: 32 });
+      this.load.spritesheet('cat_carey2_run', import.meta.env.BASE_URL + 'assets/cats/AllCatsDemo/Brown/JumpCatttt.png', { frameWidth: 32, frameHeight: 32 });
       
       // Cat 4: Calico (ThreeColor)
-      this.load.spritesheet('cat_calico_idle', '/assets/cats/AllCatsDemo/ThreeColorFree/IdleCatt.png', { frameWidth: 32, frameHeight: 32 });
-      this.load.spritesheet('cat_calico_run', '/assets/cats/AllCatsDemo/ThreeColorFree/JumpCattt.png', { frameWidth: 32, frameHeight: 32 });
+      this.load.spritesheet('cat_calico_idle', import.meta.env.BASE_URL + 'assets/cats/AllCatsDemo/ThreeColorFree/IdleCatt.png', { frameWidth: 32, frameHeight: 32 });
+      this.load.spritesheet('cat_calico_run', import.meta.env.BASE_URL + 'assets/cats/AllCatsDemo/ThreeColorFree/JumpCattt.png', { frameWidth: 32, frameHeight: 32 });
 
     // Load Tileset images for Tiled map
-    this.load.image('cielo', '/assets/tilesets/fourSeasonsPlatformer_ [tileset, version 2.0]/background_/sky_.png');
-    this.load.image('Terreno', '/assets/tilesets/fourSeasonsPlatformer_ [tileset, version 2.0]/midground_/summer_.png');
-    this.load.image('Decoraciones', '/assets/tilesets/fourSeasonsPlatformer_ [tileset, version 2.0]/objects_/staticObjects_.png');
-    this.load.spritesheet('Decoraciones_sprites', '/assets/tilesets/fourSeasonsPlatformer_ [tileset, version 2.0]/objects_/staticObjects_.png', { frameWidth: 16, frameHeight: 16 });
+    this.load.image('cielo', import.meta.env.BASE_URL + 'assets/tilesets/fourSeasonsPlatformer_ [tileset, version 2.0]/background_/sky_.png');
+    this.load.image('Terreno', import.meta.env.BASE_URL + 'assets/tilesets/fourSeasonsPlatformer_ [tileset, version 2.0]/midground_/summer_.png');
+    this.load.image('Decoraciones', import.meta.env.BASE_URL + 'assets/tilesets/fourSeasonsPlatformer_ [tileset, version 2.0]/objects_/staticObjects_.png');
+    this.load.spritesheet('Decoraciones_sprites', import.meta.env.BASE_URL + 'assets/tilesets/fourSeasonsPlatformer_ [tileset, version 2.0]/objects_/staticObjects_.png', { frameWidth: 16, frameHeight: 16 });
 
     // Load Items and Icons
     // Cat Trigger (ID 7 -> Index 6 of SuperRetroWorld icons)
-    this.load.spritesheet('retro_icons', '/assets/characters/SuperRetroWorld_CharacterPack_Full/rpgmaker/MV/characters/$_srw_free_icons1.png', { frameWidth: 48, frameHeight: 48 });
+    this.load.spritesheet('retro_icons', import.meta.env.BASE_URL + 'assets/characters/SuperRetroWorld_CharacterPack_Full/rpgmaker/MV/characters/$_srw_free_icons1.png', { frameWidth: 48, frameHeight: 48 });
     
     // Chests for the letters
-    this.load.spritesheet('chests', '/assets/characters/SuperRetroWorld_CharacterPack_Full/rpgmaker/MV/characters/_srw_free_chests.png', { frameWidth: 54, frameHeight: 96 });
+    this.load.spritesheet('chests', import.meta.env.BASE_URL + 'assets/characters/SuperRetroWorld_CharacterPack_Full/rpgmaker/MV/characters/_srw_free_chests.png', { frameWidth: 54, frameHeight: 96 });
 
   }
 
